9bba007494f17881987ddb5b4b62d9f17a675cc0
at4-kata <76214425+at4-kata@users.noreply.github.com>
2024-08-12 10:56:25 +0900

    done

---
+++ src/components/InboxScreen.stories.jsx
@@ -0,0 +1,51 @@
+import { Provider } from "react-redux";
+import InboxScreen from "./InboxScreen";
+import store from "../lib/store";
+import { MockedState } from "./TaskList.stories";
+import { http, HttpResponse } from "msw";
+import {
+  fireEvent,
+  waitFor,
+  waitForElementToBeRemoved,
+  within,
+} from "@storybook/test";
+
+export default {
+  component: InboxScreen,
+  title: "InboxScreen",
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get("https://jsonplaceholder.typicode.com/todos?userId=1", () => {
+          return HttpResponse.json(MockedState.tasks);
+        }),
+      ],
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await waitForElementToBeRemoved(await canvas.findByTestId("loading"));
+    await waitFor(async () => {
+      await fireEvent.click(canvas.getByLabelText("pinTask-1"));
+      await fireEvent.click(canvas.getByLabelText("pinTask-3"));
+      await fireEvent.click(canvas.getByLabelText("archiveTask-5"));
+    });
+  },
+};
+
+export const Error = {
+  parameters: {
+    msw: {
+      handlers: [
+        http.get("https://jsonplaceholder.typicode.com/todos?userId=1", () => {
+          return HttpResponse.json(null, { status: 403 });
+        }),
+      ],
+    },
+  },
+};
